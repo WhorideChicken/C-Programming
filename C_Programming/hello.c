5b2d251d1efffac2dad4fe8b79e4ab442ef21f37
woowon_jin@tmax.co.kr <97941811+WhorideChicken@users.noreply.github.com>
2024-05-16 15:56:59 +0900

    Feat : Loop문 예제
    - 입출력을 활용한 평균을 내는 예제

--- C_Programming/hello.c
+++ C_Programming/hello.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 void Cesius();
+void LoopExample();
 
 int main()
 {
-	Cesius();
+	LoopExample();
 	return 0;
 }
 
@@ -24,4 +25,24 @@ void Cesius()
 	scanf_s("%lf", &celsius);
 
 	printf("섭씨 %f 도는 화씨로 %f 도 입니다.\n", celsius, 9 * celsius / 5 + 32);
+}
+
+
+void LoopExample()
+{
+	int subject, score;
+	double sum = 0;
+
+	printf("몇 개의 과목 점수를 입력 받을 것인가요?");
+	scanf_s("%d", &subject);
+
+	printf("\n 각 과목의 점수를 입력해 주세요 \n");
+	for (int i = 0; i < subject; i++)
+	{
+		printf("과목 %d : ", i);
+		scanf_s("%d", &score);
+		sum += score;
+	}
+
+	printf("모든 과목의 평균은 : %.2f", sum/subject);
 }
