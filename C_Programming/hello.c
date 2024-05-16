6a0141c0574e2a78beffa5a6514d115628b61f95
woowon_jin@tmax.co.kr <97941811+WhorideChicken@users.noreply.github.com>
2024-05-16 15:32:50 +0900

    Feat : 입출력 기본 함수
    - 섭씨를 화씨로 바꿔주기

--- C_Programming/hello.c
+++ C_Programming/hello.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+void Cesius();
+
 int main()
 {
-	printf("Hello, World!\n");
+	Cesius();
 	return 0;
+}
+
+
+void Cesius()
+{
+	double celsius;
+
+	printf("섭씨 온도를 화씨 온도로 바꿔주는 함수입니다.\n");
+	printf("섭씨 온도를 입력해 주세요 : ");
+	scanf_s("%lf", &celsius);
+
+	printf("섭씨 %f 도는 화씨로 %f 도 입니다.\n", celsius, 9 * celsius / 5 + 32);
 }
