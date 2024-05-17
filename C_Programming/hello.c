65cd10af4e0eb3b98ac5c2e33ad710a9e06e84ab
woowon_jin@tmax.co.kr <97941811+WhorideChicken@users.noreply.github.com>
2024-05-17 13:18:57 +0900

    Feat : 피보나치 수열 중 짝수번째 덧셈 문제
    -  피보나치 수열 중 짝수번째 덧셈 문제

--- C_Programming/hello.c
+++ C_Programming/hello.c
@@ -4,11 +4,11 @@ void Cesius();
 void LoopExample();
 void LoopExampleSecond();
 void LoopExampThird();
-
-
+void LoopExampleFourth();
+void LoopFibonacci();
 int main()
 {
-	LoopExampThird();
+	LoopFibonacci();
 	return 0;
 }
 
@@ -29,8 +29,6 @@ void Cesius()
 
 	printf("섭씨 %f 도는 화씨로 %f 도 입니다.\n", celsius, 9 * celsius / 5 + 32);
 }
-
-
 //for(;;) -> (조건문) 에서 조건문이 없으면 항상 참이라 인식 되기 때문에 무한 루프
 //탈출 하려면 중간 조건에 break 호출
 //특정 조건을 통과하려면 continue
@@ -52,7 +50,6 @@ void LoopExample()
 
 	printf("모든 과목의 평균은 : %.2f", sum / subject);
 }
-
 //N 줄인 삼각형을 출력한다.
 //	scanf_s(" : %d", &count); 처럼 형식 지정자 앞에 :를 넣으면 오류 발생
 void LoopExampleSecond()
@@ -76,9 +73,7 @@ void LoopExampleSecond()
 	}
 
 }
-
-
-//TODO : 역피라미드
+//N 줄인 역삼각형을 출력한다.
 void LoopExampThird()
 {
 	int count;
@@ -87,7 +82,7 @@ void LoopExampThird()
 	int temp = 0;
 	for (int i = 0; i < count; i++)
 	{
-		for (int j = 0; j < (count+2)-temp; ++j)
+		for (int j = 0; j < (count + 2) - temp; ++j)
 		{
 			if (j < temp)
 				printf(" ");
@@ -99,3 +94,39 @@ void LoopExampThird()
 	}
 
 }
+//1000 이하의 3 또는 5 의 배수인 자연수들의 합을 구한다.
+void LoopExampleFourth()
+{
+	int sum = 0;
+	for (int i = 0; i <= 1000; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
+	}
+
+	printf("sum %d", sum);
+}
+
+//1000000 이하의 피보나치 수열 
+//( N 번째 항이 N - 1 번째 항과 N - 2 번째 항으로 표현되는 수열, 시작은 1,1,2,3,5,8,...) 의 짝수 항들의 합을 구한다
+void LoopFibonacci()
+{
+	int a = 1, b = 1;
+	int sum = 0;
+	int index = 2;
+
+	while (b <= 1000000) {
+		if (index % 2 == 0) {
+			sum += b;
+		}
+
+		int next = a + b;
+		a = b;
+		b = next;
+		index++;
+	}
+
+	printf("%d", sum);
+
+}
+
